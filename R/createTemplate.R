5d08f4e9cbfb999184b9870b3709c04f5d4824ab
mnel <michael.nelson@sydney.edu.au>
2016-07-22 13:26:56 +1000

    reworked and renamed functions code layout improved yaml added as dependency

--- R/createTemplate.R
+++ R/createTemplate.R
@@ -6,33 +6,36 @@
 #' @param description a short description of the template
 #' @param yamlextra list of extra details for template.yaml
 #' @param titleblockextra list of extra options for title block in rmd
+#' @importFrom yaml as.yaml
 #' @export
-create_template <- function(name ='newTemplate', description = "A new template",
-                            yamlextra = list(),titleblockextra = list()){
+createTemplate <-
+  function(name = 'newTemplate',
+           description = "A new template",
+           yamlextra = list(),
+           titleblockextra = list()) {
+    # create the directories
+    templateRoot <- templateDir(name)
+    templateSkeleton <- file.path(templateRoot, 'skeleton')
+    dir.create(templateRoot, recursive = TRUE)
+    dir.create(templateSkeleton)
+    # template.yaml
+    template_yaml <-
+      yaml::as.yaml(c(list(
+        name = name, description = description
+      ), yamlextra))
+    cat(template_yaml, file = file.path(templateRoot, 'template.yaml'))
+    #skeleton.Rmd
+    templateTitleBlock <- do.call("makeTitleBlock", titleblockextra)
+    cat(templateTitleBlock,
+        file = file.path(templateSkeleton, 'skeleton.Rmd'))
+  }
 
-  templateRoot <- templateDir(name)
-  templateSkeleton <- skeletonDir(name)
 
-  dir.create(templateRoot, recursive = TRUE)
-  dir.create(templateSkeleton)
-
-
-  template_yaml <-  yaml::as.yaml(c(list(name=name, description=description), yamlextra))
-
-  cat(template_yaml, file = file.path(templateRoot, 'template.yaml'))
-
-  titleBlock <- do.call("skeletonYAML", titleblockextra)
-  cat(titleBlock, file = file.path(templateSkeleton,'skeleton.Rmd'))
-}
-
-
-templateDir <- function(name) file.path('inst','templates',name)
-skeletonDir <- function(name) file.path('inst','templates',name,'skeleton')
-
-skeletonYAML <- function(title='Untitled', ...){
+#' A simple function to create a title block for an rmarkdown template.
+#'
+#' @param title title defaults to 'Untitled'
+#' @export
+makeTitleBlock <- function(title = 'Untitled', ...) {
   titleBlock <- sprintf('-----\n%s\n------\n',
-                        yaml::as.yaml(list(title=title, ...)))
+                        yaml::as.yaml(list(title = title, ...)))
 }
-
-
-
